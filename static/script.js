7f4237be5cbdbd09478a1022629151beeec96770
Neeraj Kumar <23f2002096@ds.study.iitm.ac.in>
2025-05-25 10:02:16 +0000

    added static folder and script and template folder and index.html

---
+++ static/script.js
@@ -0,0 +1,42 @@
+import Home from './components/Home.js'
+import Login from './components/Login.js'
+import Register from './components/Register.js'
+import AdminQuiz from './components/AdminQuiz.js'
+import Summary from './components/Summary.js'
+import Navbar from './components/Navbar.js'
+import Footer from './components/Footer.js'
+import UserDetails from './components/UserDetails.js'
+import UserQuiz from './components/UserQuiz.js'
+import Score from './components/Score.js'
+
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/login', component: Login },
+    {path:'/register', component: Register},
+    {path:'/quizzes', component: AdminQuiz},
+    {path:'/user', component: UserDetails},
+    {path:'/summary', component: Summary},
+    {path:'/quizzes/:id', component: UserQuiz},
+    {path:'/score', component: Score}
+
+]
+
+const router = new VueRouter({
+    routes // routes: routes
+})
+
+const app = new Vue({
+    el: "#app",
+    router, // router: router
+    template: `
+    <div class="container">
+        <nav-bar></nav-bar>
+        <router-view></router-view>
+    </div>
+    `,
+    components:{
+        "nav-bar": Navbar,
+    },
+    
+}) 
